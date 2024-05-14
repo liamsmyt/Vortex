5bf89e515bf30e3a484521664f1d21d7f2007c57
liamsmyt <liamsmyth2002@gmail.com>
2024-05-14 03:59:59 +0200

    color

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const cvs = document.getElementById("myCanvas");
 const ctx = cvs.getContext("2d");
-cvs.width = cvs.height = 1600;
+cvs.width = cvs.height = 1200;
 cvs.style.display = "block";
 cvs.style.margin = "0 auto";
 let divider = 0.1;
@@ -11,9 +11,15 @@ function circle(radius, distanceFromCentre, angle) {
   let x = cvs.width / 2 + distanceFromCentre * Math.sin(angle);
   let y = cvs.height / 2 + distanceFromCentre * Math.cos(angle);
 
+  let gradient = ctx.createRadialGradient(x, y, 0, x, y, radius);
+  gradient.addColorStop(0, "white"); // Start color
+  gradient.addColorStop(1, "blue"); // End color // End color
+
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.strokeStyle = gradient;
   ctx.stroke();
+  ctx.closePath();
   ctx.closePath();
 
   if (angle > 2 * Math.PI) {
@@ -26,7 +32,7 @@ function main() {
   ctx.clearRect(0, 0, cvs.width, cvs.height);
   circle(radius, 0, 0);
   if (divider < 12) divider += 0.1;
-  if (radius >= 200) radius += 1;
+  if (radius >= 200) radius += 0.5;
   animationId = requestAnimationFrame(main);
 }
 
